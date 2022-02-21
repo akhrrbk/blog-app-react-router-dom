122de6b7fe837a65cddd811d786e96b4eaca2618
Akhrorbek Abdukhamitov <akhrrbk@gmail.com>
2022-02-21 14:46:07 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import './App.css';
 import { useState, useEffect } from 'react'
+import Bloglist from './components/Bloglist';
 
 function App() {
-  const [fruit, setFruit] = useState('Anor')
-  const [date, setDate] = useState('')
+  const [blogs, setBlogs] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
-    console.log('useEffect ran');
-    console.log(fruit);
-  }, [date])
-    return (
-    <div className="App">
-      <p>{date}</p>
-      <p>{fruit}</p>
-      <button onClick={() => fruit === 'Anor' ? setFruit('Behi') : setFruit('Anor')}>change fruit name</button>
-      <button onClick={() => setDate(new Date().toLocaleTimeString())}>change dates</button>
-    </div>
+    fetch('https://ecomm-products.modus.workers.dev/')
+      .then(res => {
+        if(!res.ok){
+          throw Error('could not fetch the data from the source')
+        }
+        return res.json()
+      })
+      .then(data => {
+        setBlogs(data)
+        setIsLoading(false)
+        setError(null)
+      })
+      .catch(err => {
+        setError(err.message)
+        setIsLoading(false)
+      })
+  }, [])
+  
+  return (
+  <div className="App">
+    { error && <div>{error}</div>}
+    {isLoading && <div>Loading...</div>}
+    {blogs && (
+      blogs.splice(0,5).map(blog => (
+        <div key={blog.id}>
+          <h2>{blog.title}</h2>
+          <img src={blog.images[0]} style={{width: '20%', height: 'auto'}} alt="" />
+        </div>
+    ))
+    )}
+    {/* {blogs && <Bloglist blogs={blogs} title='All My Blogs!' />} */}
+  </div>
   );
 }
 
---
+++ src/components/Bloglist.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Bloglist = ({blogs, title}) => {
+  return (
+    <div className='blog-list'>
+        <h2>{title}</h2>
+        {blogs.map(blog => (
+            <div className='blog-preview' key={blog.id}>
+                <h2>{blog.title}</h2>
+                <p>written by {blog.author}</p>
+            </div>
+        ))}
+    </div>
+  )
+}
+
+export default Bloglist
